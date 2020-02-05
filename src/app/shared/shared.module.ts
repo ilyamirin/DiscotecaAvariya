1aaf95977ea090201656f00326d804f658b56cc3
Yuri Kolot <kolot.ius@students.dvfu.ru>
2020-02-05 13:59:40 +1000

    Add module for charts

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,27 +1,31 @@
 import {CommonModule} from '@angular/common';
 import {FormsModule} from '@angular/forms';
 import {NgModule} from '@angular/core';
-import {MatButtonModule, MatIconModule, MatTableModule, MatTooltipModule} from '@angular/material';
+import {MatButtonModule, MatGridListModule, MatIconModule, MatTableModule, MatTooltipModule} from '@angular/material';
 import {NavbarComponent} from './navbar/navbar.component';
 import {RouterModule} from '@angular/router';
-import { RussiaMapComponent } from './russia-map/russia-map.component';
-import { FooterComponent } from './footer/footer.component';
+import {RussiaMapComponent} from './russia-map/russia-map.component';
+import {FooterComponent} from './footer/footer.component';
+import {ChartsModule} from 'ng2-charts';
 
 
 @NgModule({
   imports: [
     CommonModule,
+    ChartsModule,
     FormsModule,
     RouterModule,
 
     /* Import Angular Material modules */
     MatButtonModule,
+    MatGridListModule,
     MatIconModule,
     MatTooltipModule,
     MatTableModule
   ],
   exports: [
     CommonModule,
+    ChartsModule,
     FormsModule,
 
     /* Export project components */
@@ -29,6 +33,7 @@ import { FooterComponent } from './footer/footer.component';
 
     /* Export Angular Material modules */
     MatButtonModule,
+    MatGridListModule,
     MatIconModule,
     MatTooltipModule,
     MatTableModule,
